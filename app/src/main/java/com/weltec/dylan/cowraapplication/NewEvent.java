78f5e35b8c6b50e3ef7a2a4a57d7e0499cc6ac37
tillerD <mastertiller@hotmail.co.nz>
2017-10-01 18:03:47 +1300

    Backend of Event Screen - NewEvent

--- app/src/main/java/com/weltec/dylan/cowraapplication/NewEvent.java
+++ app/src/main/java/com/weltec/dylan/cowraapplication/NewEvent.java
@@ -3,20 +3,29 @@ package com.weltec.dylan.cowraapplication;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -40,6 +49,7 @@ public class NewEvent extends Activity {
     private final Integer LOCATION = 0x1;
     private LocationManager manage;
     private LocationListener listener;
+    private HashMap people;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,8 +68,7 @@ public class NewEvent extends Activity {
         getLocation();
         lat.setText("Lat: " + Double.toString(latitude));
         lon.setText("Lon: " + Double.toString(longitude));
-        Toast.makeText(NewEvent.this, "Lat: " + latitude + " Lon: " + longitude,
-                Toast.LENGTH_SHORT).show();
+        people = new HashMap();
         spotter = (Spinner) findViewById(R.id.spotterSpinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(NewEvent.this,
                 android.R.layout.simple_spinner_item,
@@ -72,8 +81,42 @@ public class NewEvent extends Activity {
                 android.R.layout.simple_spinner_item);
         adapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         cats.setAdapter(adapt);
-        Button submitEvent = (Button) findViewById(R.id.eventBtn);
+        Button pubBtn = (Button) findViewById(R.id.publiBtn);
+        pubBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                publicPopUp(v);
+            }
+        });
+        Button propBtn = (Button) findViewById(R.id.PropertyBtn);
+        propBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                propertyPopUp(v);
+            }
+        });
+        Button vehBtn = (Button) findViewById(R.id.VehicleBtn);
+        vehBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        final Button submitEvent = (Button) findViewById(R.id.eventBtn);
         submitEvent.setText("Submit Event");
+        submitEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitEvent();
+            }
+        });
+        Button cancel = (Button) findViewById(R.id.cancelBtn);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelEvent();;
+            }
+        });
     }
 
     private String createID(Date time) {
@@ -115,10 +158,116 @@ public class NewEvent extends Activity {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {}
+                != PackageManager.PERMISSION_GRANTED) {
+        }
         manage.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
         Location loc = manage.getLastKnownLocation(LocationManager.NETWORK_PROVIDER.toString());
         longitude = loc.getLongitude();
         latitude = loc.getLatitude();
+    }
+
+    //Public popup window
+    private void publicPopUp(View v) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(NewEvent.this);
+        //Create edit fields for the pop up window
+        LinearLayout layout = new LinearLayout(NewEvent.this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        final EditText desc = new EditText(NewEvent.this);
+        desc.setHint("Person Description:");
+        desc.setHeight(50);
+        final CheckBox box = new CheckBox(NewEvent.this);
+        box.setText("BOLB");
+        layout.addView(desc);
+        layout.addView(box);
+        //Set the layout of the popup window
+        alert.setTitle("Public Person Description")
+                .setCancelable(false)
+                .setView(layout)
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+        final AlertDialog alertDialog = alert.create();
+        alertDialog.show();
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (desc.length() <= 0) {
+                            Toast.makeText(NewEvent.this, "Description Field empty!",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            if(box.isChecked()) {
+                                people.put(1, desc.getText());
+                            } else {
+                                people.put(0, desc.getText());
+                            }
+                            Toast.makeText(NewEvent.this, "Person Description added!",
+                                    Toast.LENGTH_SHORT).show();
+                            alertDialog.dismiss();
+                        }
+                    }
+                });
+    }
+
+    private void propertyPopUp(View v) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(NewEvent.this);
+        //Create edit fields for the pop up window
+        LinearLayout layout = new LinearLayout(NewEvent.this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        final EditText desc = new EditText(NewEvent.this);
+        desc.setHint("Person Description:");
+        final CheckBox box = new CheckBox(NewEvent.this);
+        box.setText("BOLB");
+        layout.addView(desc);
+        layout.addView(box);
+        //Set the layout of the popup window
+        alert.setTitle("Public Person Description")
+                .setCancelable(false)
+                .setView(layout)
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+        final AlertDialog alertDialog = alert.create();
+        alertDialog.show();
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (desc.length() <= 0) {
+                            Toast.makeText(NewEvent.this, "Description Field empty!",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            if(box.isChecked()) {
+                                people.put(1, desc.getText());
+                            } else {
+                                people.put(0, desc.getText());
+                            }
+                            Toast.makeText(NewEvent.this, "Person Description added!",
+                                    Toast.LENGTH_SHORT).show();
+                            alertDialog.dismiss();
+                        }
+                    }
+                });
+    }
+
+    private void submitEvent() {
+        events.add(id);
+        Intent intent = new Intent(NewEvent.this, Home.class);
+        intent.putExtra("IDS", (Serializable) events);
+        startActivity(intent);
+    }
+
+    private void cancelEvent() {
+        Intent intent = new Intent(NewEvent.this, Home.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // do nothing.
     }
 }
